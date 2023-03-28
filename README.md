07abfc4f92addf4fc7f59ff04cdb3da21370e150
Antonio War <antonioguerra.it@gmail.com>
2023-03-28 17:45:26 +0200

    fix: remove max_tokens parameter from each call and request, bug will solved soon

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ SwiftyGPT is a simple and lightweight wrapper around OpenAI API.
 It was born with the aim of provide a Swift like interface around all OpenAI capabilities, so you can make requestes and get responses with minimal coding effort.
 
 - **Easy to use:** no configuration needed, SwiftyGPT is ready to go, you just need an API Key.
-- **Configurable:** you can choose some parameters like model, temperature, tokens or just use default values.
+- **Configurable:** you can choose some parameters like model, temperature or just use default values.
 - **SwiftUI compatible:** provides an elegant SwiftUI support that is very easy to use.
 
 ---
--- Sources/SwiftyGPT/SwiftyGPT.swift
+++ Sources/SwiftyGPT/SwiftyGPT.swift
@@ -20,8 +20,8 @@ public class SwiftyGPT: ObservableObject {
         
     // MARK: - Chat
     
-    public func chat(messages: [SwiftyGPTMessage], model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, choices: Int = 1, tokens: Int = Int.max, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil, completion: @escaping (Result<SwiftyGPTResponse, Error>) -> ()) {
-        let request = SwiftyGPTRequest(messages: messages, model: model, temperature: temperature, choices: choices, stream: false, tokens: tokens, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user)
+    public func chat(messages: [SwiftyGPTMessage], model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, choices: Int = 1, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil, completion: @escaping (Result<SwiftyGPTResponse, Error>) -> ()) {
+        let request = SwiftyGPTRequest(messages: messages, model: model, temperature: temperature, choices: choices, stream: false, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user)
         SwiftyHTTP.request(with: SwiftyGPTRouter.chat(apiKey, request), body: SwiftyGPTResponse.self) { result in
             switch result {
             case .success(let response):
@@ -34,24 +34,24 @@ public class SwiftyGPT: ObservableObject {
         }
     }
     
-    public func chat(messages: [SwiftyGPTMessage], model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, choices: Int = 1, tokens: Int = Int.max, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil) async -> Result<SwiftyGPTResponse, Error> {
+    public func chat(messages: [SwiftyGPTMessage], model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, choices: Int = 1, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil) async -> Result<SwiftyGPTResponse, Error> {
         return await withCheckedContinuation { continuation in
-            chat(messages: messages, model: model, temperature: temperature, choices: choices, tokens: tokens, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user) { result in
+            chat(messages: messages, model: model, temperature: temperature, choices: choices, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user) { result in
                 continuation.resume(returning: result)
             }
         }
     }
     
-    public func chat(message: SwiftyGPTMessage, model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, choices: Int = 1, tokens: Int = Int.max, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil, completion: @escaping (Result<SwiftyGPTResponse, Error>) -> ()) {
-        chat(messages: [message], model: model, temperature: temperature, choices: choices, tokens: tokens, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user, completion: completion)
+    public func chat(message: SwiftyGPTMessage, model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, choices: Int = 1, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil, completion: @escaping (Result<SwiftyGPTResponse, Error>) -> ()) {
+        chat(messages: [message], model: model, temperature: temperature, choices: choices, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user, completion: completion)
     }
     
-    public func chat(message: SwiftyGPTMessage, model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, choices: Int = 1, tokens: Int = Int.max, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil) async -> Result<SwiftyGPTResponse, Error> {
-        await chat(messages: [message], model: model, temperature: temperature, choices: choices, tokens: tokens, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user)
+    public func chat(message: SwiftyGPTMessage, model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, choices: Int = 1, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil) async -> Result<SwiftyGPTResponse, Error> {
+        await chat(messages: [message], model: model, temperature: temperature, choices: choices, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user)
     }
     
-    public func chat(messages: [String], model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, tokens: Int = Int.max, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil, completion: @escaping (Result<String, Error>) -> ()) {
-        chat(messages: messages.map({SwiftyGPTMessage(content: $0)}), model: model, temperature: temperature, tokens: tokens, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user) { result in
+    public func chat(messages: [String], model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil, completion: @escaping (Result<String, Error>) -> ()) {
+        chat(messages: messages.map({SwiftyGPTMessage(content: $0)}), model: model, temperature: temperature, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user) { result in
             switch result {
             case .success(let response):
                 guard let message = response.choices.first?.message else {
@@ -65,19 +65,19 @@ public class SwiftyGPT: ObservableObject {
         }
     }
     
-    public func chat(messages: [String], model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, tokens: Int = Int.max, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil) async -> Result<String, Error> {
+    public func chat(messages: [String], model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil) async -> Result<String, Error> {
         return await withCheckedContinuation { continuation in
-            chat(messages: messages, model: model, temperature: temperature, tokens: tokens, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user) { result in
+            chat(messages: messages, model: model, temperature: temperature, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user) { result in
                 continuation.resume(returning: result)
             }
         }
     }
     
-    public func chat(message: String, model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, tokens: Int = Int.max, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil, completion: @escaping (Result<String, Error>) -> ()) {
-        chat(messages: [message], model: model, temperature: temperature, tokens: tokens, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user, completion: completion)
+    public func chat(message: String, model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil, completion: @escaping (Result<String, Error>) -> ()) {
+        chat(messages: [message], model: model, temperature: temperature, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user, completion: completion)
     }
     
-    public func chat(message: String, model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, tokens: Int = Int.max, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil) async -> Result<String, Error> {
-        await chat(messages: [message], model: model, temperature: temperature, tokens: tokens, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user)
+    public func chat(message: String, model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil) async -> Result<String, Error> {
+        await chat(messages: [message], model: model, temperature: temperature, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user)
     }
 }
