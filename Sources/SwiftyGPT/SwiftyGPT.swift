78c152011234360b27bdaf5e6ab577db1813ad8c
Antonio War <antonioguerra.it@gmail.com>
2023-03-28 16:30:17 +0200

    feat: add chat string interfaces

--- Sources/SwiftyGPT/SwiftyGPT.swift
+++ Sources/SwiftyGPT/SwiftyGPT.swift
@@ -18,6 +18,8 @@ public class SwiftyGPT: ObservableObject {
         self.conversation = []
     }
         
+    // MARK: - Chat
+    
     public func chat(messages: [SwiftyGPTMessage], model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, choices: Int = 1, tokens: Int = Int.max, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil, completion: @escaping (Result<SwiftyGPTResponse, Error>) -> ()) {
         let request = SwiftyGPTRequest(messages: messages, model: model, temperature: temperature, choices: choices, stream: false, tokens: tokens, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user)
         SwiftyHTTP.request(with: SwiftyGPTRouter.chat(apiKey, request), body: SwiftyGPTResponse.self) { result in
@@ -46,5 +48,36 @@ public class SwiftyGPT: ObservableObject {
     
     public func chat(message: SwiftyGPTMessage, model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, choices: Int = 1, tokens: Int = Int.max, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil) async -> Result<SwiftyGPTResponse, Error> {
         await chat(messages: [message], model: model, temperature: temperature, choices: choices, tokens: tokens, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user)
+    }
+    
+    public func chat(messages: [String], model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, tokens: Int = Int.max, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil, completion: @escaping (Result<String, Error>) -> ()) {
+        chat(messages: messages.map({SwiftyGPTMessage(content: $0)}), model: model, temperature: temperature, tokens: tokens, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user) { result in
+            switch result {
+            case .success(let response):
+                guard let message = response.choices.first?.message else {
+                    completion(.failure(URLError(.badServerResponse)))
+                    return
+                }
+                completion(.success(message.content))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    public func chat(messages: [String], model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, tokens: Int = Int.max, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil) async -> Result<String, Error> {
+        return await withCheckedContinuation { continuation in
+            chat(messages: messages, model: model, temperature: temperature, tokens: tokens, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user) { result in
+                continuation.resume(returning: result)
+            }
+        }
+    }
+    
+    public func chat(message: String, model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, tokens: Int = Int.max, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil, completion: @escaping (Result<String, Error>) -> ()) {
+        chat(messages: [message], model: model, temperature: temperature, tokens: tokens, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user, completion: completion)
+    }
+    
+    public func chat(message: String, model: SwiftyGPTModel = .stable, @Ranged(0...2) temperature: Float = 1, tokens: Int = Int.max, @Ranged(-2.0...2.0) presencePenalty: Float = 0, @Ranged(-2.0...2.0) frequencyPenalty: Float = 0, user: String? = nil) async -> Result<String, Error> {
+        await chat(messages: [message], model: model, temperature: temperature, tokens: tokens, presencePenalty: presencePenalty, frequencyPenalty: frequencyPenalty, user: user)
     }
 }
