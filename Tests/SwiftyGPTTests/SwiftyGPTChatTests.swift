13a79270f464f18f33eea105c5be9860796a4ac1
Antonio War <antonioguerra.it@gmail.com>
2023-03-28 17:54:55 +0200

    test: all chat string methods

--- Tests/SwiftyGPTTests/SwiftyGPTChatTests.swift
+++ Tests/SwiftyGPTTests/SwiftyGPTChatTests.swift
@@ -6,7 +6,7 @@ final class SwiftyGPTChatTests: XCTestCase {
     
     override func setUpWithError() throws {
         try super.setUpWithError()
-        swiftyGPT = SwiftyGPT(apiKey: "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")
+        swiftyGPT = SwiftyGPT(apiKey: "")
     }
 
     override func tearDownWithError() throws {
@@ -29,6 +29,60 @@ final class SwiftyGPTChatTests: XCTestCase {
     
     func testDefaultAsync() async {
         let result: Result<SwiftyGPTResponse, Error> = await swiftyGPT.chat(messages: [SwiftyGPTMessage(role: .user, content: "Hi, how are you?")])
+        XCTAssertNoThrow(try result.get())
+    }
+    
+    func testSingleMessageCompletion() {
+        let expectation = expectation(description: "SingleMessageCompletion")
+        var result: Result<SwiftyGPTResponse, Error>? = nil
+        swiftyGPT.chat(message: SwiftyGPTMessage(role: .user, content: "Hi, how are you?")) { response in
+            result = response
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 30, handler: nil)
+        
+        XCTAssertNotNil(result)
+        XCTAssertNoThrow(try result?.get())
+    }
+    
+    func testSingleMessageAsync() async {
+        let result: Result<SwiftyGPTResponse, Error> = await swiftyGPT.chat(message: SwiftyGPTMessage(role: .user, content: "Hi, how are you?"))
+        XCTAssertNoThrow(try result.get())
+    }
+    
+    func testStringCompletion() {
+        let expectation = expectation(description: "StringCompletion")
+        var result: Result<String, Error>? = nil
+        swiftyGPT.chat(messages: ["Hi how are you ?", "I'm SwiftyGPT"]) { response in
+            result = response
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 30, handler: nil)
+        
+        XCTAssertNotNil(result)
+        XCTAssertNoThrow(try result?.get())
+    }
+    
+    func testStringAsync() async {
+        let result: Result<String, Error> = await swiftyGPT.chat(messages: ["Hi how are you ?", "I'm SwiftyGPT"])
+        XCTAssertNoThrow(try result.get())
+    }
+    
+    func testSingleStringCompletion() {
+        let expectation = expectation(description: "SingleStringCompletion")
+        var result: Result<String, Error>? = nil
+        swiftyGPT.chat(message: "Hi how are you ?") { response in
+            result = response
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 30, handler: nil)
+        
+        XCTAssertNotNil(result)
+        XCTAssertNoThrow(try result?.get())
+    }
+    
+    func testSingleStringAsync() async {
+        let result: Result<String, Error> = await swiftyGPT.chat(message: "Hi how are you ?")
         XCTAssertNoThrow(try result.get())
     }
 }
