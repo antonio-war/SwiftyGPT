b6e436a8317af3690c22b2f31a8f1732aa7c4f5e
Antonio War <antonioguerra.it@gmail.com>
2023-03-28 11:35:39 +0200

    fix: SwiftyHTTP import

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyGPT",
-    platforms: [.macOS(.v13)],
+    platforms: [.macOS(.v13), .iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftyGPT",
-            dependencies: []),
+            dependencies: ["SwiftyHTTP"]),
         .testTarget(
             name: "SwiftyGPTTests",
             dependencies: ["SwiftyGPT", "SwiftyHTTP"]),
