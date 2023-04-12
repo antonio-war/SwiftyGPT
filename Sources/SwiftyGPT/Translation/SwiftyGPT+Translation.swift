bd9031904e1f7fee81c6ecadba5d2dbe4c248332
Antonio War <antonioguerra.it@gmail.com>
2023-04-12 15:44:34 +0200

    feat: create translation methods

--- Sources/SwiftyGPT/Translation/SwiftyGPT+Translation.swift
+++ Sources/SwiftyGPT/Translation/SwiftyGPT+Translation.swift
@@ -9,11 +9,9 @@ import Foundation
 
 extension SwiftyGPT {
     
-    public func translation(text: String, from: SwiftyGPTLanguage, to: SwiftyGPTLanguage, user: String? = nil, completion: @escaping (Result<String, Error>) -> ()) {
-        
-        let prompt = "Translate this from \(from.rawValue.capitalized) to \(to.rawValue.capitalized): \n\(text.trimmingCharacters(in: .whitespacesAndNewlines))"
-        
-        self.completion(prompt: prompt, model: .text_davinci_003, temperature: 0.3, presencePenalty: 0.0, frequencyPenalty: 0.0) { result in
+    public func translation(text: String, from: SwiftyGPTLanguage? = nil, to: SwiftyGPTLanguage, user: String? = nil, completion: @escaping (Result<String, Error>) -> ()) {
+    
+        self.completion(prompt: prompt(text: text, from: from, to: to), model: .text_davinci_003, temperature: 0.3, presencePenalty: 0.0, frequencyPenalty: 0.0, user: user) { result in
             switch result {
                 case .success(let response):
                     if let text = response.choices.first?.text {
@@ -25,5 +23,20 @@ extension SwiftyGPT {
                     completion(.failure(error))
             }
         }
+    }
+    
+    public func translation(text: String, from: SwiftyGPTLanguage? = nil, to: SwiftyGPTLanguage, user: String? = nil) async -> Result<String, Error> {
+        return await withCheckedContinuation { continuation in
+            translation(text: text, from: from, to: to) { result in
+                continuation.resume(returning: result)
+            }
+        }
+    }
+    
+    private func prompt(text: String, from: SwiftyGPTLanguage? = nil, to: SwiftyGPTLanguage) -> String {
+        guard let from else {
+            return "Translate this into \(to.rawValue.capitalized): \n\(text.trimmingCharacters(in: .whitespacesAndNewlines))"
+        }
+        return "Translate this from \(from.rawValue.capitalized) into \(to.rawValue.capitalized): \n\(text.trimmingCharacters(in: .whitespacesAndNewlines))"
     }
 }
