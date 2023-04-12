1726a27334176bec24ddab4a4ce03400ed3e7259
Antonio War <antonioguerra.it@gmail.com>
2023-04-12 14:59:26 +0200

    feat: create SwiftyGPTLanguage enum

---
+++ Sources/SwiftyGPT/Utils/SwiftyGPTLanguage.swift
@@ -0,0 +1,53 @@
+//
+//  SwiftyGPTLanguage.swift
+//  
+//
+//  Created by Antonio Guerra on 12/04/23.
+//
+
+import Foundation
+
+enum SwiftyGPTLanguage: String, Codable {
+    case afrikaans
+    case arabic
+    case armenian
+    case azerbaijani
+    case belarusian
+    case bosnian
+    case bulgarian
+    case catalan
+    case chinese
+    case croatian
+    case czech
+    case danish
+    case dutch
+    case english
+    case estonian
+    case finnish
+    case french
+    case galician
+    case german
+    case greek
+    case hebrew
+    case hindi
+    case hungarian
+    case icelandic
+    case indonesian
+    case italian
+    case japanese
+    case kannada
+    case kazakh
+    case korean
+    case latvian
+    case spanish
+    case swahili
+    case swedish
+    case tagalog
+    case tamil
+    case thai
+    case turkish
+    case ukrainian
+    case urdu
+    case vietnamese
+    case welsh
+}
