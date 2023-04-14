5af76e67e96e896e993530c219f7a94a477ab66b
Antonio War <antonioguerra.it@gmail.com>
2023-04-14 10:12:26 +0200

    feat: add intValue to SwiftyGPTSentiment

--- Sources/SwiftyGPT/Sentiment/SwiftyGPTSentiment.swift
+++ Sources/SwiftyGPT/Sentiment/SwiftyGPTSentiment.swift
@@ -11,4 +11,15 @@ public enum SwiftyGPTSentiment: String, Codable {
     case positive
     case neutral
     case negative
+    
+    var intValue: Int {
+        switch self {
+        case .positive:
+            return 1
+        case .neutral:
+            return 0
+        case .negative:
+            return -1
+        }
+    }
 }
