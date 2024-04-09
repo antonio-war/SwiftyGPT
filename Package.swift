5ddc42d69d32531fc1bcc67f0beceb304f12bde1
Antonio War <antonioguerra.it@gmail.com>
2024-04-09 20:47:09 +0200

    refactor: remove dependencies

--- Package.swift
+++ Package.swift
@@ -5,25 +5,28 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftyGPT",
-    platforms: [.macOS(.v13), .iOS(.v13)],
+    platforms: [
+        .macOS(.v13),
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "SwiftyGPT",
-            targets: ["SwiftyGPT"]),
+            targets: ["SwiftyGPT"]
+        ),
     ],
     dependencies: [
-        .package(url: "https://github.com/antonio-war/SwiftyHTTP", from: "0.1.5"),
-        .package(url: "https://github.com/antonio-war/SwiftyRanged", from: "1.0.0")
     ],
     targets: [
         .target(
             name: "SwiftyGPT",
-            dependencies: ["SwiftyHTTP", "SwiftyRanged"]),
+            dependencies: [
+            ]
+        ),
         .testTarget(
             name: "SwiftyGPTTests",
-            dependencies: ["SwiftyGPT", "SwiftyHTTP"],
-            resources: [
-                .copy("Utils/OpenAI-Info.plist")
-            ])
+            dependencies: [
+            ]
+        )
     ]
 )
