abfd35a27e0e117644ccd74a98aedff6d23328d8
Antonio War <antonioguerra.it@gmail.com>
2024-04-14 18:46:27 +0200

    test: add some SwiftyGPTChatMessage tests

--- Tests/SwiftyGPTChatTests/Models/SwiftyGPTChatMessageTests.swift
+++ Tests/SwiftyGPTChatTests/Models/SwiftyGPTChatMessageTests.swift
@@ -10,11 +10,31 @@ import XCTest
 
 final class SwiftyGPTChatMessageTests: XCTestCase {
     
+    var encoder: JSONEncoder!
+    
+    override func setUpWithError() throws {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        self.encoder = encoder
+    }
+    
+    override func tearDownWithError() throws {
+        self.encoder = nil
+    }
+    
     func testSystemMessageInitWhenNameIsNil() throws {
         let message = SwiftyGPTChatSystemMessage(content: "Test")
         XCTAssertEqual(message.role, .system)
         XCTAssertEqual(message.content, "Test")
         XCTAssertEqual(message.name, nil)
+    }
+    
+    func testSystemMessageEncodeWhenNameIsNil() throws {
+        let message = SwiftyGPTChatSystemMessage(content: "Test")
+        let encodedMessage = try encoder.encode(message)
+        let encodedString = String(data: encodedMessage, encoding: .utf8)
+        let expectedString = "{\n  \"role\" : \"system\",\n  \"content\" : \"Test\"\n}"
+        XCTAssertEqual(encodedString, expectedString)
     }
     
     func testSystemMessageInitWhenNameIsNotNil() throws {
