1a347ac0245bee7418b8bd6914670e6af48c7c62
Antonio War <antonioguerra.it@gmail.com>
2024-04-15 20:25:33 +0200

    fix: remove not ready tests

--- Sources/SwiftyGPTChat/Models/SwiftyGPTChatResponse.swift
+++ Sources/SwiftyGPTChat/Models/SwiftyGPTChatResponse.swift
@@ -10,4 +10,15 @@ import Foundation
 enum SwiftyGPTChatResponse: Equatable {
     case success(body: SwiftyGPTChatResponseSuccessBody)
     case failure(body: SwiftyGPTChatResponseFailureBody)
+    
+    init(body: any SwiftyGPTChatResponseBody) throws {
+        switch body {
+        case let body as SwiftyGPTChatResponseSuccessBody:
+            self = .success(body: body)
+        case let body as SwiftyGPTChatResponseFailureBody:
+            self = .failure(body: body)
+        default:
+            throw URLError(.cannotParseResponse)
+        }
+    }
 }
