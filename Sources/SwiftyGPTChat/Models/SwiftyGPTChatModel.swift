f1cbcc863029a7830641c7259bd47f4016180b1e
Antonio War <antonioguerra.it@gmail.com>
2024-04-26 22:24:45 +0200

    feat: add gpt4 models

--- Sources/SwiftyGPTChat/Models/SwiftyGPTChatModel.swift
+++ Sources/SwiftyGPTChat/Models/SwiftyGPTChatModel.swift
@@ -8,12 +8,19 @@
 import Foundation
 
 public enum SwiftyGPTChatModel: String, Codable {
-    case gpt3_5_turbo = "gpt-3.5-turbo"
-    case gpt3_5_turbo_16k = "gpt-3.5-turbo-16k"
-    case gpt3_5_turbo_16k_0613 = "gpt-3.5-turbo-16k-0613"
-    case gpt3_5_turbo_0301 = "gpt-3.5-turbo-0301"
-    case gpt3_5_turbo_0613 = "gpt-3.5-turbo-0613"
-    case gpt3_5_turbo_1106 = "gpt-3.5-turbo-1106"
+    case gpt4_turbo = "gpt-4-turbo"
+    case gpt4_turbo_preview = "gpt-4-turbo-preview"
+    case gpt4_0125_preview = "gpt-4-0125-preview"
+    case gpt4_1106_preview = "gpt-4-1106-preview"
+    case gpt4 = "gpt-4"
+    case gpt4_0613 = "gpt-4-0613"
+    case gpt4_32k = "gpt-4-32k"
+    case gpt4_32k_0613 = "gpt-4-32k-0613"
     case gpt3_5_turbo_0125 = "gpt-3.5-turbo-0125"
-    // TODO: add gpt4 models
+    case gpt3_5_turbo = "gpt-3.5-turbo"
+    case gpt3_5_turbo_0301 = "gpt-3.5-turbo-0301"
+    case gpt3_5_turbo_1106 = "gpt-3.5-turbo-1106"
+    case gpt3_5_turbo_16k = "gpt-3.5-turbo-16k"
+    case gpt3_5_turbo_0613 = "gpt-3.5-turbo-0613"
+    case gpt3_5_turbo_16k_0613 = "gpt-3.5-turbo-16k-0613"
 }
--- Tests/SwiftyGPTChatTests/Models/SwiftyGPTChatModelTests.swift
+++ Tests/SwiftyGPTChatTests/Models/SwiftyGPTChatModelTests.swift
@@ -10,6 +10,46 @@ import XCTest
 
 final class SwiftyGPTChatModelTests: XCTestCase {
     
+    func testGpt4_turboInitFromRawValue() throws {
+        let model = try XCTUnwrap(SwiftyGPTChatModel(rawValue: "gpt-4-turbo"))
+        XCTAssertEqual(model, .gpt4_turbo)
+    }
+    
+    func testGpt4_turbo_previewInitFromRawValue() throws {
+        let model = try XCTUnwrap(SwiftyGPTChatModel(rawValue: "gpt-4-turbo-preview"))
+        XCTAssertEqual(model, .gpt4_turbo_preview)
+    }
+    
+    func testGpt4_0125_previewInitFromRawValue() throws {
+        let model = try XCTUnwrap(SwiftyGPTChatModel(rawValue: "gpt-4-0125-preview"))
+        XCTAssertEqual(model, .gpt4_0125_preview)
+    }
+    
+    func testGpt4_1106_previewInitFromRawValue() throws {
+        let model = try XCTUnwrap(SwiftyGPTChatModel(rawValue: "gpt-4-1106-preview"))
+        XCTAssertEqual(model, .gpt4_1106_preview)
+    }
+    
+    func testGpt4InitFromRawValue() throws {
+        let model = try XCTUnwrap(SwiftyGPTChatModel(rawValue: "gpt-4"))
+        XCTAssertEqual(model, .gpt4)
+    }
+    
+    func testGpt4_0613InitFromRawValue() throws {
+        let model = try XCTUnwrap(SwiftyGPTChatModel(rawValue: "gpt-4-0613"))
+        XCTAssertEqual(model, .gpt4_0613)
+    }
+    
+    func testGpt4_32kInitFromRawValue() throws {
+        let model = try XCTUnwrap(SwiftyGPTChatModel(rawValue: "gpt-4-32k"))
+        XCTAssertEqual(model, .gpt4_32k)
+    }
+    
+    func testGpt4_32k_0613InitFromRawValue() throws {
+        let model = try XCTUnwrap(SwiftyGPTChatModel(rawValue: "gpt-4-32k-0613"))
+        XCTAssertEqual(model, .gpt4_32k_0613)
+    }
+    
     func testGpt3_5_turboInitFromRawValue() throws {
         let model = try XCTUnwrap(SwiftyGPTChatModel(rawValue: "gpt-3.5-turbo"))
         XCTAssertEqual(model, .gpt3_5_turbo)
