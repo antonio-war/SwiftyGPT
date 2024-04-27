bc07e9db8145e02758850d66d3283152f2c63045
Antonio War <antonioguerra.it@gmail.com>
2024-04-27 20:33:34 +0200

    build: add empty explorer project

---
+++ Explorer/Explorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Explorer
+//
+//  Created by Antonio Guerra on 27/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Explorer/Explorer/ExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplorerApp.swift
+//  Explorer
+//
+//  Created by Antonio Guerra on 27/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
